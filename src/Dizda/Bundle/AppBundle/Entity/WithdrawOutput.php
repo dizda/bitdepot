dfa7ca7a2b90e8d88db865d9bb0566840ec4b997
Jonathan Dizdarevic <dizzda@gmail.com>
2014-11-06 17:56:03 +0100

    [W.I.P] withdraw : entities created

---
+++ src/Dizda/Bundle/AppBundle/Entity/WithdrawOutput.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Dizda\Bundle\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * WithdrawOutput
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class WithdrawOutput
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="amount", type="decimal")
+     */
+    private $amount;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="to_address", type="string", length=255)
+     */
+    private $toAddress;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="is_accepted", type="boolean")
+     */
+    private $isAccepted;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="reference", type="string", length=255)
+     */
+    private $reference;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set amount
+     *
+     * @param string $amount
+     * @return WithdrawOutput
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return string 
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Set toAddress
+     *
+     * @param string $toAddress
+     * @return WithdrawOutput
+     */
+    public function setToAddress($toAddress)
+    {
+        $this->toAddress = $toAddress;
+
+        return $this;
+    }
+
+    /**
+     * Get toAddress
+     *
+     * @return string 
+     */
+    public function getToAddress()
+    {
+        return $this->toAddress;
+    }
+
+    /**
+     * Set isAccepted
+     *
+     * @param boolean $isAccepted
+     * @return WithdrawOutput
+     */
+    public function setIsAccepted($isAccepted)
+    {
+        $this->isAccepted = $isAccepted;
+
+        return $this;
+    }
+
+    /**
+     * Get isAccepted
+     *
+     * @return boolean 
+     */
+    public function getIsAccepted()
+    {
+        return $this->isAccepted;
+    }
+
+    /**
+     * Set reference
+     *
+     * @param string $reference
+     * @return WithdrawOutput
+     */
+    public function setReference($reference)
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    /**
+     * Get reference
+     *
+     * @return string 
+     */
+    public function getReference()
+    {
+        return $this->reference;
+    }
+}
