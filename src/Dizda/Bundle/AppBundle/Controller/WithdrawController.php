7bc6f34bd46742d20ae80ea20a37947fed2b4407
Jonathan Dizdarevic <dizzda@gmail.com>
2014-11-12 23:36:55 +0100

    [W.I.P] front : lighter theme

--- src/Dizda/Bundle/AppBundle/Controller/WithdrawController.php
+++ src/Dizda/Bundle/AppBundle/Controller/WithdrawController.php
@@ -3,8 +3,6 @@
 namespace Dizda\Bundle\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class WithdrawController extends Controller
 {
