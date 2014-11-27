3dd0be0723f96a5c969661c7029fa1c9576be9a2
Jonathan Dizdarevic <dizzda@gmail.com>
2014-11-27 12:26:46 +0100

    poc

--- node/poc.js
+++ node/poc.js
@@ -1,38 +1,23 @@
 var bitcoin = require('bitcoinjs-lib');
 
 
-var rawTransaction = '01000000014c80a5129d03d0ebe28c2626fb2b35962c438f548ff399cc85b462109338550b0100000000ffffffff0210270000000000001976a914d356d4d8079f8556be4c8102ecf00cc63344e75488ac307500000000000017a9145010eb90f22f4865766e969fc5e07c1e2445f81a8700000000';
-var redeemScriptHex = '522102a6074afbed684715760e4eebe9878fea1ea3862828e687674a841d7e03db07c7210253468fb6d90c4d9bf6d02dde920dc7cfa214b3103fc0f59b7c52a4163df6bbaf210210ec8ae465d9d6a686a8718e9051b581277ee05889fc7776a2925a111c7fc77f53ae';
-var redeemScriptASM = '0 3045022100f68727afbd82d8b0d3a6c2161db8d3e049c1627307a994c18b33b65dc3c9814502207bb4998766fe0c5fcbdf260b2175bd70895c30d8d51419678c2a7c2e1c21f87701 30440220305c2da7a189f3213b282c486af680a20ef1bb39860ec7bbbe034ee5837e25690220375ebed5ef0a9c6c556b9e535b58567d86b179103eedd887c62155ef2682e48d01 522103b367219a55e4d9a74f508f05ad376535fe653a60453545d828bdd095c1f11eea210283f6c706be826c63b3e1f8d4de345e4eab581015544507025cc2772627d4da00210379a6f8d1e4d376dc00eeb96fc02219ac0d7811773c2a9050889e346836cb435c53ae';
-var privKeys = [
-    'KwLPiuHXJGkuyMJkjybUH8M2nQ9A64FYXMtT9RFJLxgtT5wZBAey',
-    'KxjVtJZscdLZ1GZR8cHhhDo8bXMmQNjST3f4FW2AVhCFS7h4k8q2'
+var rawTransaction = '0100000002490148e331b17b540e4e50e99537d30058a13c070771f4a572d10ee49f23f32a0100000000ffffffff2f8f8eebc1425458217d57f710459aef4b672fe63d969646a0b7ac8c178252be0000000000ffffffff02204e0000000000001976a914d356d4d8079f8556be4c8102ecf00cc63344e75488ac307500000000000017a9146bacd88c6c3cd4d2bc5d5e85ca39cc246aac945f8700000000';
+var redeemScriptHex = '522102a71ef05b31072d778b35f47d6204b80733db964498267f61dec2bdaaca22752121025bcd11b34f89704aba4d8f88d5e4d5db2a65ed1d6aabbc1f335f2eec771ee4e421024da9e2fb260317954c54df92d78051ef230de9a5aafef2592bb3b4f666209bcf53ae';
+var privKeysSignature0 = [
+    'KxpeZuPLQhLHk7LMiEcGaoZMCJP6pJrocyVbjurYsNmB3KdXDABD', // input:0
+    'KzUvPH8vGidQuEnjdBxSrKM687o5aRAarLaYhjTX3SZXymAMJVv9'  // input:1
 ].map(function(wif) { return bitcoin.ECKey.fromWIF(wif) });
 
-// Recover transaction from raw_transaction created by bitcoind || or raw_signed_transaction
+var privKeysSignature1 = [
+    'L2pVbMKhRvk3RSTJtj4jXVqThrh2E9AXrJ5Prc6THG391gVbP9om', // input:0
+    'KyNJyUHi6h88nLydvgrnxbg1JCgSYvFgj1mPwxbeL35XJSqzKToa'  // input:1
+].map(function(wif) { return bitcoin.ECKey.fromWIF(wif) });
+
 var tx  = bitcoin.Transaction.fromHex(rawTransaction);
 
 // Build it into Transaction Builder
 var txb = bitcoin.TransactionBuilder.fromTransaction(tx);
-//var txb = new bitcoin.TransactionBuilder();
-//txb.addInput('35f383c45b94f0307e03777c855f1d9b5eeba13c1f8eca2ba275ea2b7fba3631', 1);
-//txb.addOutput('1LGTbdVSEbD9C37qXcpvVJ1egdBu8jYSeV', 10000);
-//txb.addOutput('3MQmgtgNgvPQQpfFc7ENbN2hJmi3iaoVWm', 50000);
 
-// Create a wallet from the seed submitted
-//var walletDIZDA = new bitcoin.Wallet(bitcoin.crypto.sha256('bambou1'), bitcoin.networks.bitcoin);
-//var walletZENOU = new bitcoin.Wallet(bitcoin.crypto.sha256('bambou2'), bitcoin.networks.bitcoin);
-//var walletSERVER = new bitcoin.Wallet(bitcoin.crypto.sha256('bambou3'), bitcoin.networks.bitcoin);
-//
-//var pubkeys = [];
-//pubkeys.push(walletDIZDA.getInternalAccount().derive(12).pubKey);
-//pubkeys.push(walletZENOU.getInternalAccount().derive(12).pubKey);
-//pubkeys.push(walletSERVER.getInternalAccount().derive(12).pubKey);
-
-
-//console.log(privKeys[0].pub);
-//var redeemScript = bitcoin.scripts.multisigOutput(2, pubkeys);
-//console.log(redeemScript.toHex());
 
 txb.sign(0, privKeys[0], bitcoin.Script.fromHex(redeemScriptHex));
 txb.sign(0, privKeys[1], bitcoin.Script.fromHex(redeemScriptHex));
