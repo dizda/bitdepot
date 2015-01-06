7d60f63faa3955338203e1ae26e55f90079fc1a5
Jonathan Dizdarevic <dizzda@gmail.com>
2015-01-06 19:16:40 +0100

    fix tests

--- src/Dizda/Bundle/AppBundle/Tests/BaseFunctionalTestController.php
+++ src/Dizda/Bundle/AppBundle/Tests/BaseFunctionalTestController.php
@@ -34,7 +34,7 @@ class BaseFunctionalTestController extends WebTestCase
         $this->client->startIsolation();
 
         // Login as dizda via JWT Token
-        $this->client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE0MTkzNDYwNTcsInVzZXJuYW1lIjoiZGl6ZGEiLCJpYXQiOiIxNDE5MjU5NjU3In0.v446yY7SvuFRGtO6qPJreVVWXyBwThPf6O5FjMrbITq5P9WbBSg7IugIIZXZr0iobb59fXoJl9KYM42SHRQxkuWkEj3Fpifz99K_CTwKU9Asb02TTqcwu_k5L7GhixW106A-SvXBNc2l6a54xLyB2YrEPdk9PaszIHtr3i2vwxM1PgFWoMJNOkE6mCHvlPWXlEHDqEVg9JW1Lb9o5OzwCMNy4L5OIA_rGOfvwlk7mHKbohXrj6Da5QMFAdhNRqpL4XXNCUsa_fPzQAem0WiEl5wr8kTkJwmgJSqZVFqGAYUb7TWq8LbCGJCWpsEcSfKEl57nUwNFdgi9Me-nMIUXnCKDP5g2PKDx7JSraKix9l7W50hFNP20-iVZAkqND5ZQdTNH34P5W2bKl6OZwpx3OKsgs4YJ672H5ZHEaKWuqQhk1GrwHPMGPNleG43SE_6ur_SjcjAwj3jmDhT-wYO-WHWAiDkgH2q8AviInplJ6i0xOd18vSiGwMYQXSm3rHCb_eGOskzxa7TquR1-4I56JHe03IfNMPkOIz-5ouvXUi2IORCda8scmiQrpq5_bt-KO0IKRpDQDgPdDbemOf7cWTNxMpqC7sVwWI2oQB5pRHxCsbxFqyUFcrJor71gBDYDS5cESLuysOEXxfIJ4Ci-M7QmIk_yHpz4Adomu5s3XlQ'));
+        $this->client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE0MjA2NTQ1MTIsInVzZXJuYW1lIjoiZGl6ZGEiLCJpYXQiOiIxNDIwNTY4MTEyIn0.nAyAA0w2NfLbYxl5LQfzchA8zHSuuZph2kpb87EKahh_MUKX73wdab2PkH4PIJZwSE7QeDTRxxEddTC8OyuQIpWYF5btlgZ8qyUcyeTCcuLhk3KmmBH8XTBceLRjgzc55aicc0pgw4gnLdUfeYLuak-WauA2QkC3VAmVgNi0oa1lfJyMv_EaPPSQZjWIC5cAUJwAFdGff_FLGcwl8zk5cwL9jaOIOAcPKYa_E0PtZNnrgBwV7y8wjYeTBLWwsghJzlBNjGgTBSB-cchSVQ7_bYIHtUoAmISIvRZbRiJI6T-9zHwgSf_VVH0bR4ePUKeaumBy_hgikItcoX9vQVVOsOCYTbhRV-B4rAjGzXYmUMvN66tKD8UnvfjoY67VGT4Jx_9gs2YIl_nPfGDrFG5Nea3krqDvHpiJeTLdAmmiXqop282Ew2X7GpPO4RBQ1gBITPdI6mTKN-8mC0HoDFs9zkP94YxkVBI2EKEofYwl-W0IeVD521uNOOFVmPBjukfCtGPxneSUiFti4-lrfQ4-G4NjssbvBIOaZ7CvsxOKuZQzLhI-y-Z_oxjvqiFsenIbIrWfeyySqbw-nCQcdpzxQcV287nottz3JB94Se2_xJgwC4qQNq2TOVqk_LXjfj7OsCK1egbhgM1rmtyk2rh1J1ypnFApAwPHAhF4vjVWyWs'));
 
 //        // Regenerate token
 //        $this->client->request(
@@ -47,7 +47,7 @@ class BaseFunctionalTestController extends WebTestCase
 //        );
 //
 //        $data = json_decode($this->client->getResponse()->getContent(), true);
-////        echo $data['token']; // Token
+//        echo PHP_EOL . $data['token'] . PHP_EOL; // Token
 //        $this->client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
     }
 
