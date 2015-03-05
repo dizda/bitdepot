7d07221eb62f02cff5ff302324fc010ba51d7b76
Jonathan Dizdarevic <dizzda@gmail.com>
2015-03-05 14:17:50 +0100

    sign: fix public key

--- node/poc_bitcore_sign_tx2.js
+++ node/poc_bitcore_sign_tx2.js
@@ -5,17 +5,17 @@
 var bitcore = require('bitcore')
   , Hash = bitcore.crypto.Hash;
 
+
 var pubkey1 = [
     new bitcore.PublicKey('0208c207e1a5122e77afacfd2a1eaae48f8be879e4a8beda1a4bc83db8171834d7'),
     new bitcore.PublicKey('02180c60b560029109b4ca936435b15e26598a44c5b7bf9bd3017736cfd81404db'),
     new bitcore.PublicKey('03f90d8bc82e05a024e5990429605265ec21c038c5d673584ebaf1362d65aad7da')
 ];
 var pubkey2 = [
-    new bitcore.PublicKey('0208c207e1a5122e77afacfd2a1eaae48f8be879e4a8beda1a4bc83db8171834d7'),
-    new bitcore.PublicKey('02180c60b560029109b4ca936435b15e26598a44c5b7bf9bd3017736cfd81404db'),
-    new bitcore.PublicKey('03f90d8bc82e05a024e5990429605265ec21c038c5d673584ebaf1362d65aad7da')
+    new bitcore.PublicKey('02090dd9ec73b8ddc0cce7826040c56b80b8e33abfab4b5d9ea2a0e444e6c74185'),
+    new bitcore.PublicKey('02b106f270cc598f47300b7d475337b96b3be7e88b8e360abbad1b5d3a888268bc'),
+    new bitcore.PublicKey('03d8610409add92f1d4917a08069ba38bf0b1c241109c1ebe9dc57fe33ff544dd1')
 ];
-
 
 var address1 = new bitcore.Address(pubkey1, 2);
 var address2 = new bitcore.Address(pubkey2, 2);
@@ -47,15 +47,15 @@ var transaction = new bitcore.Transaction()
 //    .change(new bitcore.Address('3AH75iWPWvZ8cN8kMGdDuG7GjSEJzTVJiK'))
     .sign('L43XZrsKz8h2zyEWJxyx27VZ9Zje7CijgZ8ZjUe6xn7gny86HfE3')
     .sign('L2wxUn3Y9CBSzmcD9KHUkVZ8zPs84HtnRCRJtZiyjKzHxxbPB5Nk')
-    .sign('KzAP1PMQvsBYNnFrntBBZ2NeYsfuWpxTq5yQgANs3EPfxk4UvMcp')
-    .sign('L3yTikaT5vh3kh3waFQV37v9Qnu4BXK9m4pPFjqjzBYtvpwGaNWt')
+    //.sign('KzAP1PMQvsBYNnFrntBBZ2NeYsfuWpxTq5yQgANs3EPfxk4UvMcp')
+    //.sign('L3yTikaT5vh3kh3waFQV37v9Qnu4BXK9m4pPFjqjzBYtvpwGaNWt')
 ;
 
-console.log(transaction);
+//console.log(transaction);
 
 //console.log(transaction.toObject());
 //console.log(transaction.toJSON());
-console.log(transaction.serialize());
+//console.log(transaction.serialize());
 
 //console.log('Count signatures: %d', transaction.countSignatures());
 //console.log('Count missing signatures: %d', transaction.countMissingSignatures());
